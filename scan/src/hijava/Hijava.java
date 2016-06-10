69731c31015ab364a681a711cc3c6a84022c4ab7
ckan2010 <ckan2010@gmail.com>
2016-06-10 17:04:09 +0900

    안녕 자바

---
+++ scan/src/hijava/Hijava.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package hijava;
+
+/**
+ * @date   :2016. 6. 10.
+ * @author :ckan
+ * @file   :Hijava.java
+ * @story  :
+ */
+public class Hijava {
+	public static void main(String[] args) {
+		System.out.println("Hi java");
+	}
+
+}
