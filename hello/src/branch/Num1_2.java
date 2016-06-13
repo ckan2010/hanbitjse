55a7f5abc6b8b68e259063faf448091228933a85
ckan2010 <ckan2010@gmail.com>
2016-06-13 18:19:16 +0900

    for 문 연습 예제1

--- hello/src/branch/Num1_2.java
+++ hello/src/branch/Num1_2.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  * @file   :Num1.java
  * @story  :
  */
-public class Num1 {
+public class Num1_2 {
 	/**
 	 * 저희반은 학생수가 3명입니다.
 	 * 학생의 평균점수는 0~100점 사이입니다.
@@ -35,10 +35,15 @@ public class Num1 {
 	 * */
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String name1="",name2="",name3="",first="",second="",therd="",
+		String name="",name1="",name2="",name3="",
 			   one="",two="",three="";
-		int    avg1=0,avg2=2,avg3=0,result1=0,result2=0,result3=0;
-		System.out.println("3명 의 학생이름 과 평균점수를 입력 하세요 : ");
+		int    avg=0,avg1=0,avg2=2,avg3=0,result1=0,result2=0,result3=0,i=0;
+		
+		for (i = 1; i <= 3; i++) {
+			System.out.println("이름,점수 입력");
+			name = scanner.next();
+		    avg  = scanner.nextInt();
+		}
 	    name1 = scanner.next();
 	    avg1  = scanner.nextInt();
 	    if (avg1>100 || avg1<0){
@@ -54,63 +59,48 @@ public class Num1 {
 	    name3 = scanner.next();
 	    avg3  = scanner.nextInt();
 	    if (avg3>100 || avg3<0){
-	    	System.out.println("학생2 의 평균점수는 0~100점 사이입니다.");
+	    	System.out.println("학생3 의 평균점수는 0~100점 사이입니다.");
 	    	return;
 	    }
 	    if(avg1 > avg2 && avg1 > avg3) {
-			first = "1등";
 			one = name1;
 			result1 = avg1;
 			if (avg2 > avg3 ) {
-				second = "2등";
 				two   = name2;
 				result2 = avg2;
-				therd = "3등";
 				three = name3;
 				result3 = avg3;
 			} else{
-				second = "2등";
 				two   = name3;
 				result2 = avg3;
-				therd = "3등";
 				three = name2;
 				result3 = avg2;
 			}
 		} else if(avg2 > avg1 && avg2 > avg3) {
-				first = "1등";
 				one = name2;
 				result1 = avg2;
 				if (avg1 > avg3 ) {
-					second = "2등";
 					two   = name1;
 					result2 = avg1;
-					therd = "3등";
 					three = name3;
 					result3 = avg3;
 				} else{
-					second = "2등";
 					two   = name3;
 					result2 = avg3;
-					therd = "3등";
 					three = name1;
 					result3 = avg1;
 				}
 			} else if(avg3 > avg1 && avg3 > avg2) {
-					first = "1등";
 					one = name3;
 					result1 = avg3;
 					if (avg1 > avg2 ) {
-						second = "2등";
 						two   = name1;
 						result2 = avg1;
-						therd = "3등";
 						three = name2;
 						result3 = avg2;
 					} else{
-						second = "2등";
 						two   = name2;
 						result2 = avg2;
-						therd = "3등";
 						three = name1;
 						result3 = avg1;
 					}
@@ -119,9 +109,9 @@ public class Num1 {
 	    	return;
 		}
 	    System.out.println("===================================");
-	    System.out.println(first+" "+one+"  "+result1+" 점");
-	    System.out.println(second+" "+two+"  "+result2+" 점");
-	    System.out.println(therd+" "+three+"  "+result3+" 점");
+	    System.out.println("1등 "+one+"  "+result1+" 점");
+	    System.out.println("2등 "+two+"  "+result2+" 점");
+	    System.out.println("3등 "+three+"  "+result3+" 점");
 	    System.out.println("===================================");
 	    
 	}
